ba5f23fd25ea6b5ded9ee831179a5e4a5b68ccc4
tabishismail <tabish554@gmail.com>
2021-05-28 20:40:49 +0500

    Chapter # 31 to 34 Assignmet # 3 Done

--- app.js
+++ app.js
@@ -10,4 +10,11 @@
 
 // var monthName= ["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
 // var time = new Date().getMonth();
-// console.log(monthName[time]);
+// alert("Current Month : "+ monthName[time]);
+
+// Assignment # 3
+
+// var dayName= ["Sun","Mon","Tues","Weds","Thurs","Fri","Sat"];
+// var time = new Date().getDay();
+// alert("Today is "+ dayName[time]);
+
